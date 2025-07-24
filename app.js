868c978cf300a387c996acb3af02cb4a65aba2bd
sabihkahn <whyonlyhead@gmail.com>
2025-07-24 13:56:03 +0500

    push code fixed

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.get('/', async (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('server successfully run')
-})
+// app.listen(3000, () => {
+//     console.log('server successfully run')
+// })
 
 export default app
